c828ff68b19e5e36c22a4190c1b79408b7a760bf
Alfonso Gastelum-Strozzi <alfonso.gastelum@comunidad.unam.mx>
2024-03-15 11:38:21 -0600

    correction save

---
+++ Tnet/Tnet/Migrations/20240315173609_upd4.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class upd4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HablalenguaIndigena",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PertenecegrupoEtnico",
+                table: "Patients");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HablalenguaIndigena",
+                table: "Patients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PertenecegrupoEtnico",
+                table: "Patients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
